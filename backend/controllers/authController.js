56ead49fd3dd191053b45c6d0d2f80547bfd6344
hafeez-07 <hafeezmohammad689@gmail.com>
2026-03-22 11:47:23 +0530

    fix(auth): return user data after successful registration

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,34 +5,36 @@ import { generateToken } from "../utils/generateToken.js";
 
 export const registerUser = async (req, res) => {
   try {
-    const { username, email, password } = req.body;
+    const { fullname, username, email, password } = req.body;
 
     //validate input
     if (!username || !email || !password) {
-      res.status(400).json({
+      return res.status(400).json({
         error: "All fields are required",
       });
     }
 
-    //if username exist
-    const usernameExist = await User.findOne({ username });
-    if (usernameExist) {
+    //if user exist
+
+    const existingUser = await User.findOne({
+      $or: [{ email }, { username }],
+    });
+
+    if (existingUser) {
       return res.status(400).json({
-        error: "Username is not available",
+        error:
+          existingUser.email == email
+            ? "Email is already registered"
+            : "Username is not available",
       });
     }
 
-    //if email is already registered
-    const emailExist = await User.findOne({ email });
-    if (emailExist) {
-      return res.status(404).json({
-        error: "Email is already registered",
-      });
-    }
-
-    //else create a new user
+    //hash password
     const hashedPassword = await bcrypt.hash(password, 10);
+
+    //create new user
     const createdUser = await User.create({
+      fullname,
       username,
       email,
       password: hashedPassword,
@@ -41,9 +43,10 @@ export const registerUser = async (req, res) => {
     //generateToken
     generateToken(createdUser._id, res);
 
-    res.status(201).json({
-      msg: "Registration succesful",
-    });
+    //make password undefined before sending
+    createdUser.password = undefined;
+
+    res.status(201).json(createdUser);
   } catch (err) {
     res.status(500).json({
       error: err.message,
