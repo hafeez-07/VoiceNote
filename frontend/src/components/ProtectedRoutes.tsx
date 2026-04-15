f51322f04c3c9cbe67ac4ed413bedd75032dd2d3
hafeez-07 <hafeezmohammad689@gmail.com>
2026-04-15 22:08:54 +0530

    feat(ui): Implement skeleton component for loading state in protectedRoutes

--- frontend/src/components/ProtectedRoutes.tsx
+++ frontend/src/components/ProtectedRoutes.tsx
@@ -1,11 +1,12 @@
 import useAuth from "../../hooks/useAuth";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
+import Skeleton from "./Skeleton";
 
 const ProtectedRoutes = () => {
   const { loading, user } = useAuth();
   const location = useLocation();
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <Skeleton />;
   if (!user) {
     return (
       <Navigate
